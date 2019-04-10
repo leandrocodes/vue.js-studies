641ae346f2f788c1398aa15cf8609751e80ca854
Leandro <leaxviana140@gmail.com>
2019-04-10 12:54:44 -0400

    add item list

---
+++ vue-fire/vuecrud/src/components/EditItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Edit Item</h1>
+</template>
+
+<script>
+export default {
+  components: {
+      name: 'EditItem'
+  }
+}
+</script>
---
+++ vue-fire/vuecrud/src/components/ListItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>List Item</h1>
+</template>
+
+<script>
+export default {
+  components: {
+      name: 'ListItem'
+  }
+}
+</script>
