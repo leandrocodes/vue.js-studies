41168a910be21943405e8dc253983319516f378f
Leandro <leaxviana140@gmail.com>
2019-05-06 19:35:45 -0400

    attack  & special attack

--- Monster Killer/monster.js
+++ Monster Killer/monster.js
@@ -15,8 +15,24 @@ new Vue({
             this.running = true
             this.playerLife = 100
             this.monsterLife = 100
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        damage(atr, min, max, especial){
+            const plus  = especial ? 5 : 0
+            const damage = this.getRandom(min + plus, max + plus)
+            this[atr] = Math.max(this[atr] - damage, 0)
+        },
+            attack(special){
+            this.damage('playerLife', 7, 12, false)
+            this.damage('monsterLife', 5, 10, special)
         }
     },
     watch: {
+        hasResult(value){
+            if(value) this.running = false
+        }
     },
 })
