5d0928c72d92bf1148d431389e98e3c958a1e8d6
Leandro <leaxviana140@gmail.com>
2019-05-06 18:47:05 -0400

    placar / buttons

--- Monster Killer/monster.js
+++ Monster Killer/monster.js
@@ -1,11 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        playerLife: 10,
+        playerLife: 100,
         monsterLife: 100,
     },
     computed: {
-    
+        hasResult(){
+            return this.playerLife === 0 || this.monsterLife === 0
+        }
     },
     methods: {
 
