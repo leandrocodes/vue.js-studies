e2c9b1e7114a28ecaf1e14b76c61e2977fd014b8
Leandro <leaxviana140@gmail.com>
2019-05-06 12:50:53 -0400

    1st challenge - 2nd section

---
+++ 2nd section/desafio 1/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		expressao: true,
+		nomes: ['Ana', 'Rafa', 'Lucas', 'Gui'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		}
+	}
+});
