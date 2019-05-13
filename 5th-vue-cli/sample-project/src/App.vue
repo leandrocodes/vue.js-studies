20fbe23211fdeb09fdf4d20b4152501cd49f722c
Leandro <leaxviana140@gmail.com>
2019-05-12 20:27:50 -0400

    vue cli example

--- 5th-vue-cli/sample-project/src/App.vue
+++ 5th-vue-cli/sample-project/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        {{ titulo }}
+        <button @click="titulo += '#'">Alterar</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                titulo: 'Leandro Viana'
+            }
+        },
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
+    #app{
+        background-color: chocolate;
+
+    }
 </style>
