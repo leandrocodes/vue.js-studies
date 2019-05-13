1ab34bf7a74828fbba555a88aa1839c2004e76ac
Leandro <leaxviana140@gmail.com>
2019-05-13 17:25:16 -0400

    scaffold

--- 6th-components-introduction/component-intro/src/App.vue
+++ 6th-components-introduction/component-intro/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Contadores</h1>
+        {{contador}}
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return{
+            contador: 0
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
