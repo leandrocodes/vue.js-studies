909b4ba3fab5a334c7beba819e3af4466c013c94
Leandro <leaxviana140@gmail.com>
2019-05-13 17:40:48 -0400

    create a component.vue in src folder

--- 6th-components-introduction/component-intro/src/App.vue
+++ 6th-components-introduction/component-intro/src/App.vue
@@ -11,7 +11,7 @@ export default {
         return{
             contador: 0
         }
-    }
+    }   
 }
 </script>
 
---
+++ 6th-components-introduction/component-intro/src/Contador.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="contador">
+        <span>{{contador}}</span>
+        <button @click="adicionar">++</button>
+        <button @click="remover">--</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return { contador: 0 }
+    },
+    methods: {
+        adicionar(){
+            this.contador++
+        },
+        remover(){
+            this.contador--
+        }
+    },
+}
+</script>
+
+<style>
+    
+    .contador span{
+        border-bottom: 1px solid #CCC;
+        height: 30vh;
+        padding: 5px 25px;
+    }
+
+    .contador button{
+        height: 30px;
+        width: 30px;
+        border-radius: 50%;
+        background-color: blueviolet;
+        color: #FFF;
+        margin-left: 10px;
+        outline: none;
+    }
+
+</style>
