9eba5a32e704d9a4808ac13031dee31e04827c57
Leandro <leaxviana140@gmail.com>
2019-05-14 19:00:27 -0400

    user details

--- 7.1th - comunication-app-slots/challenge/src/components/UsuarioDetalhe.vue
+++ 7.1th - comunication-app-slots/challenge/src/components/UsuarioDetalhe.vue
@@ -1,9 +1,17 @@
 <template>
+    <div class="usuario-detalhe">
+        <h2 v-if="!usuario">Usuário não selecionado!</h2>
+        <div v-else>
+            <h1>ID: {{ usuario.id }} </h1>
+            <h2>Nome: {{ usuario.nome }} </h2>
+            <h2>Idade: {{usuario.idade}} </h2>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-
+    
 }
 </script>
 
