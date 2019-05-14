ce13fc2e703d16aea7688204b1ba94b3b057e26c
Leandro <leaxviana140@gmail.com>
2019-05-14 19:05:14 -0400

    starter version

--- 7.1th - comunication-app-slots/challenge/src/components/UsuarioDetalhe.vue
+++ 7.1th - comunication-app-slots/challenge/src/components/UsuarioDetalhe.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="usuario-detalhe">
-        <h2 v-if="!usuario">Usuário não selecionado!</h2>
-        <div v-else>
-            <h1>ID: {{ usuario.id }} </h1>
-            <h2>Nome: {{ usuario.nome }} </h2>
-            <h2>Idade: {{usuario.idade}} </h2>
-        </div>
+  <div class="usuario-detalhe">
+    <h2 v-if="!usuario">Usuário não selecionado!</h2>
+    <div v-else>
+      <h1>ID: {{ usuario.id }}</h1>
+      <h2>Nome: {{ usuario.nome }}</h2>
+      <h2>Idade: {{usuario.idade}}</h2>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
-
+.usuario-detale {
+  flex: 1;
+  border: 1px solid #ccc;
+}
 </style>
--- 7.1th - comunication-app-slots/challenge/src/components/UsuarioLista.vue
+++ 7.1th - comunication-app-slots/challenge/src/components/UsuarioLista.vue
@@ -1,13 +1,45 @@
 <template>
-    
+  <div class="usuario-lista">
+    <table>
+      <thead>
+        <tr>
+          <th>#ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="usuario in usuarios" :key="usuario.id">
+          <td>{{ usuario.id }}</td>
+          <td>{{ usuario.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.usuario-lista{
+    flex: 1;
+    display: flex;
+}
+
+table{
+    flex: 1;
+    border-spacing: 0px;
+    border-collapse: separate;
+    border-top: 1px solid #ccc;
+}
+
+th, td{
+    padding: 15px;
+    border-bottom: 1px solid #ccc;
+}
+tbody tr:hover{
+    background: #00000001
+}
 
 </style>
