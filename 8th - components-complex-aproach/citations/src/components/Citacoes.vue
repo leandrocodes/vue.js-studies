c73a667194bac30ba9754d73a9c5b34a3cb48fa1
Leandro <leaxviana140@gmail.com>
2019-05-14 18:13:46 -0400

    data via slot, giving every reference of an object in a array

--- 8th - components-complex-aproach/citations/src/components/Citacoes.vue
+++ 8th - components-complex-aproach/citations/src/components/Citacoes.vue
@@ -1,52 +1,58 @@
 <template>
-    <div class="citacoes">
-        <span>
-            <button @click="numero--">&lt;</button>
-            <button @click="numero++">&gt;</button>
-        </span>
-        <Citacao>
-            <p>{{citacoes[indice].texto}}</p>
-        </Citacao>
-    </div>
+  <div class="citacoes">
+    <span>
+      <button @click="numero--">&lt;</button>
+      <button @click="numero++">&gt;</button>
+    </span>
+    <Citacao>
+        <h1> {{ citacoes[indice].autor }} </h1>
+        <p> {{ citacoes[indice].texto }} </p>
+        <p style="font-size:12px"> {{ citacoes[indice].fonte }} </p>
+    </Citacao>
+  </div>
 </template>
 
 <script>
-
-import Citacao from './Citacao.vue'
+import Citacao from "./Citacao.vue";
 
 export default {
-    components: {Citacao},
-    data() {
-        return {
-            numero: 0,
-            citacoes: [{
-                fonte: 'Jornal do Empreendedor',
-                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
-                autor: 'Margaret Mead'
-            }, { 
-                fonte: 'Frases de Mãe',
-                texto: 'Isso não é um quarto, é um chiqueiro.',
-                autor: 'Roberta'
-            }, {
-                fonte: 'Frases de Pai',
-                texto: 'Vou contar até 3! 1, 2, 2...',
-                autor: 'Gustavo'
-            }]
+  components: { Citacao },
+  data() {
+    return {
+      numero: 0,
+      citacoes: [
+        {
+          fonte: "Jornal do Empreendedor",
+          texto:
+            "Lembre-se sempre que você é absolutamente único. Assim como todos os outros.",
+          autor: "Margaret Mead"
+        },
+        {
+          fonte: "Frases de Mãe",
+          texto: "Isso não é um quarto, é um chiqueiro.",
+          autor: "Roberta"
+        },
+        {
+          fonte: "Frases de Pai",
+          texto: "Vou contar até 3! 1, 2, 2...",
+          autor: "Gustavo"
         }
-    },
-    computed: {
-        indice() {
-            return Math.abs(this.numero % 3)
-            console.log(indice)
-        }
+      ]
+    };
+  },
+  computed: {
+    indice() {
+      return Math.abs(this.numero % 3);
+      console.log(indice);
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-    .citacoes {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+.citacoes {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
