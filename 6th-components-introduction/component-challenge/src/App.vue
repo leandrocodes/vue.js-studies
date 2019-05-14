7cc3fd369848ca7afdabe664046ebd7a0df51d84
Leandro <leaxviana140@gmail.com>
2019-05-13 20:34:36 -0400

    @/components

--- 6th-components-introduction/component-challenge/src/App.vue
+++ 6th-components-introduction/component-challenge/src/App.vue
@@ -16,10 +16,10 @@
 </template>
 
 <script>
-import Menu from "./Menu.vue"
-import Toolbar from "./Toolbar.vue"
-import Carousel from "./Carousel.vue"
-import Footer from "./Footer.vue"
+import Menu from "@/components/template/Menu.vue"
+import Toolbar from "@/components/template/Toolbar.vue"
+import Carousel from "@/components/widgets/Carousel.vue"
+import Footer from "@/components/template/Footer.vue"
 
 export default {
   components: {
