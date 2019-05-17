7dd7e0c86920105c7a3fc6499162cd703855cc33
Leandro <leaxviana140@gmail.com>
2019-05-17 14:00:43 -0400

    new gradient

--- 9th - todo-list/src/App.vue
+++ 9th - todo-list/src/App.vue
@@ -26,16 +26,16 @@ export default {
 <style>
 body {
   font-family: "Lato", sans-serif;
-  background: #4776e6; /* fallback for old browsers */
+  background: #2b5876; /* fallback for old browsers */
   background: -webkit-linear-gradient(
-    to right,
-    #4776e6,
-    #8e54e9
+    to left,
+    #4e4376,
+    #2b5876
   ); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(
-    to right,
-    #4776e6,
-    #8e54e9
+    to left,
+    #4e4376,
+    #2b5876
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
   color: #fff;
