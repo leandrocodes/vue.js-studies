d7030825d50aa717ea0c41d7de9356326010cfa0
Leandro <leaxviana140@gmail.com>
2019-05-17 15:22:09 -0400

    style add new task

--- 9th - todo-list/src/components/NewTask.vue
+++ 9th - todo-list/src/components/NewTask.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="new-task">
-        <input v-model="name" type="text" @keydown.enter="add">
-        <button @click="add">+</button>
+        <input class="form-element" placeholder="Adicione uma nova tarefa! 😀" v-model="name" type="text" @keydown.enter="add">
+        <button class="form-element" @click="add">✔️</button>
     </div>
 </template>
 
@@ -22,5 +22,27 @@ export default {
 </script>
 
 <style>
-
+    .new-task{
+        margin: 2rem;
+    }
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid rgba(255, 255, 255, 0);
+        padding: 5px 10px 8px;
+        color: rgb(49, 49, 49);
+    }
+    input.form-element{
+        width: 500px;
+        background: rgba(255, 255, 255, 0.493);
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+        border-right: none;
+    }
+    button{
+        border-left: none;
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+        background-color: rgb(171, 241, 250);
+    }
 </style>
