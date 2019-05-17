d2c774ca635d4b102e8aeda3b1184770c0244d3c
Leandro <leaxviana140@gmail.com>
2019-05-17 13:58:52 -0400

    component task grid and show all the tasks via props

--- 9th - todo-list/src/App.vue
+++ 9th - todo-list/src/App.vue
@@ -1,34 +1,58 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-	</div>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <task-grid :tasks="tasks"></task-grid>
+  </div>
 </template>
 
 <script>
-export default {
+import TaskGrid from "./components/TaskGrid";
 
-}
+export default {
+  components: {
+    TaskGrid
+  },
+  data() {
+    return {
+      tasks: [
+        { name: "Lavar Louça", pending: false },
+        { name: "Comprar comida", pending: true }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: #4776e6; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #4776e6,
+    #8e54e9
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #4776e6,
+    #8e54e9
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+  color: #fff;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
---
+++ 9th - todo-list/src/components/TaskGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tasks">
+    <template>
+      <div v-for="task in tasks" :key="task.name">{{task.name}}</div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
