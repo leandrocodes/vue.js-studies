0e82a29ffe2e634764774a74c7646c7c83837bb7
Leandro <leaxviana140@gmail.com>
2019-05-17 21:25:11 -0400

    style taskbar

--- 9th - todo-list/src/App.vue
+++ 9th - todo-list/src/App.vue
@@ -63,12 +63,12 @@ body {
         to left,
         #4e4376,
         #2b5876
-    ); /* Chrome 10-25, Safari 5.1-6 */
+    );
     background: linear-gradient(
         to left,
         #4e4376,
         #2b5876
-    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    );
 
     color: #fff;
 }
--- 9th - todo-list/src/components/TasksProgress.vue
+++ 9th - todo-list/src/components/TasksProgress.vue
@@ -14,4 +14,28 @@ export default {
 </script>
 
 <style>
+    .tasks-progress{
+        position: relative;
+        width: 80%;
+        border: 1px solid #fff;
+        color: #fff;
+        border-radius: 4px;
+        margin-bottom: 15px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .progress-bar{
+        position: absolute;
+        border-radius: 4px;
+        height: 100%;
+        background-color: #2980b9;
+        align-self: flex-start;
+    }
+
+    .progress-value{
+        z-index: 1;
+    }
+
 </style>
