f3fab4c1192f925c7038bee4af215175453ecf98
Leandro <leaxviana140@gmail.com>
2019-07-31 15:55:55 -0400

    manipulando as props dentro dos componentes filhos

--- revisão/4 componentes/communication/src/components/UsuarioInfo.vue
+++ revisão/4 componentes/communication/src/components/UsuarioInfo.vue
@@ -2,13 +2,18 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do usuário: {{ usuarioNome }} </p>
+        <p>Nome do usuário: {{ inverterNome() }} </p>
     </div>
 </template>
 
 <script>
 export default {
-    props:['usuarioNome']
+    props:['usuarioNome'],
+    methods: {
+        inverterNome(){
+            return this.usuarioNome.split('').reverse().join('')
+        }
+    }
 }
 </script>
 
