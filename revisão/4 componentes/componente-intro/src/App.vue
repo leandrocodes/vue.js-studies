8bbf47c22fd83a9f557e90c1dbb2079e052f1b91
Leandro <leaxviana140@gmail.com>
2019-07-31 13:50:01 -0400

    contadores

---
+++ revisão/4 componentes/componente-intro/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container">
+		<h1>Contadores</h1>
+		<p>{{contador}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			contador: 0
+		}
+	}
+}
+</script>
+
+<style>
+</style>
