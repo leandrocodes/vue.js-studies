3da7a9896ed298547c9733187adfbbd3abac9723
Leandro <leaxviana140@gmail.com>
2019-07-31 14:55:41 -0400

    carousel component

--- revisão/4 componentes/componente-desafio/src/App.vue
+++ revisão/4 componentes/componente-desafio/src/App.vue
@@ -2,12 +2,9 @@
 <v-app>
 
     <Menu></Menu>
+	<Toolbar></Toolbar>
+	<Carousel></Carousel>
 
-    <v-content>
-        <v-carousel>
-            <v-carousel-item v-for="(item,i) in fotos" :key="i" :src="item.src"></v-carousel-item>
-        </v-carousel>
-    </v-content>
 
     <v-footer class="pa-3" app>
         <v-spacer></v-spacer>
@@ -18,27 +15,16 @@
 
 <script>
 import Menu from './Menu'
+import Toolbar from './Toolbar'
+import Carousel from './Carousel'
+
 export default {
 
     components: {
-        Menu
+		Menu,
+		Toolbar,
+		Carousel
     },
-    data() {
-        return {
-            fotos: [{
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
-                },
-                {
-                    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
-                }
-            ]
-        }
-    }
+
 }
 </script>
