bf3aa5df59a30c226c17463d7bea897f913fcb52
Leandro <leaxviana140@gmail.com>
2019-08-08 14:42:46 -0400

    task grid flex

---
+++ revisão/6 todo/src/components/Task.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="task" :class="stateClass">
+        <p>
+            {{task.name}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: { type: Object, required: true }
+    },
+    computed:{
+        stateClass(){
+            return{
+                pending: this.task.pending,
+                done: !this.task.pending
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .task{
+        box-sizing: border-box;
+        width: 350px;
+        height: 150px;
+        padding: 10px;
+        border-radius: 5px;
+        font-size: 2rem;
+        font-weight: 300;
+        cursor: pointer;
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .pending{
+        border-left: 12px solid rgb(211, 39, 39);
+        background-color: rgb(199, 64, 64);
+    }
+
+    .done{
+        border-left: 12px solid rgb(44, 130, 180);
+        background-color: rgb(70, 161, 214);
+        text-decoration: line-through;
+    }
+</style>
--- revisão/6 todo/src/components/TaskGrid.vue
+++ revisão/6 todo/src/components/TaskGrid.vue
@@ -1,21 +1,39 @@
 <template>
-    <div class="tasks">
-        <template>
-            <div v-for="task in tasks" :key="task.name">
-                {{task.name}}
-            </div>
-        </template>
-    </div>
+	<div class="task-grid">
+		<template v-if="tasks.length">
+			<Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+		</template>
+		<p v-else class="no-task">Sua vida está em dia!</p>
+	</div>
 </template>
 
 <script>
+import Task from "./Task"
 export default {
-    props:{
-        tasks: {type: Array, required: true}
-    }
+	props: {
+		tasks: { type: Array, required: true }
+	},
+	components: {
+		Task
+	}
 }
 </script>
 
 <style>
+    .task-grid{
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+
+    }
+
+    .task-grid .task{
+        margin: 10px;
+    }
+
+    .no-task{
+        color: #aaaaaa;
+        font-size: 1.7rem;
+    }
 
 </style>
