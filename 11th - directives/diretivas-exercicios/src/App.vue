2d7ddc5b947c24a25b489f81b86882f3f6839130
Leandro <leaxviana140@gmail.com>
2019-08-11 01:24:26 -0400

    2 modifiers in 1 directive

--- 11th - directives/diretivas-exercicios/src/App.vue
+++ 11th - directives/diretivas-exercicios/src/App.vue
@@ -1,35 +1,44 @@
 <template>
 	<div id="app">
 		<h1>Diretivas</h1>
-		<hr>
+		<hr />
 		<p v-text="'Usando diretiva v-text'"></p>
 		<p v-html="'Usando <strong>v-html</strong>'"></p>
-		<hr>
+		<hr />
 		<p v-destaque.atrasar="'cyan'">Usando diretiva personalizada</p>
 		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
-		<hr>
+		<hr />
 		<p v-destaque-local.atrasar="'green'">Usando diretiva personalizada</p>
-		<p v-destaque-local:fundo.atrasar="'lightgreen'">Usando diretiva personalizada</p>	
-			
+		<p v-destaque-local:fundo.atrasar.alternar="'lightgreen'">Usando diretiva personalizada</p>
 	</div>
 </template>
 
 <script>
 export default {
-	directives:{
-		'destaque-local':{
-			bind(el, binding){
-		//el.style.backgroundColor = 'lightgreen'
+	directives: {
+		"destaque-local": {
+			bind(el, binding) {
+				const aplicarCor = cor => {
+					if (binding.arg === "fundo") el.style.backgroundColor = cor
+					else el.style.color = cor
+				}
+
 				let atraso = 0
 
-				if(binding.modifiers['atrasar'])
-					atraso = 3000
-				
-				setTimeout(()=>{
-					if(binding.arg === 'fundo')
-						el.style.backgroundColor = binding.value
-					else
-						el.style.color = binding.value
+				if (binding.modifiers["atrasar"]) atraso = 3000
+
+				const cor1 = binding.value
+				const cor2 = 'purple'
+				let corAtual = cor1
+
+				setTimeout(() => {
+					if (binding.modifiers["alternar"]) {
+						setInterval(()=>{
+							corAtual = corAtual === cor1 ? cor2 : cor1
+							aplicarCor(corAtual)
+						}, 1000)
+					} else
+						aplicarCor(binding.value)
 				}, atraso)
 			}
 		}
@@ -39,7 +48,7 @@ export default {
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
