15766688d445f86b75a4097eeb9870f48de85407
Leandro <leaxviana140@gmail.com>
2019-08-11 00:12:39 -0400

    directives

---
+++ 11th - directives/diretivas-exercicios/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<hr>
+		<p v-text="'Usando diretiva v-text'"></p>
+		<p v-html="'Usando <strong>v-html</strong>'"></p>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
---
+++ 11th - directives/diretivas-exercicios/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.directive('destaque',{
+	
+})
+
+new Vue({
+	render: h => h(App),
+}).$mount('#app')
