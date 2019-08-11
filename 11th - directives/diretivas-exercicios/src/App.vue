beb27800f4e0dee60854452f88cf006092840ef2
Leandro <leaxviana140@gmail.com>
2019-08-11 01:04:44 -0400

    local directives

--- 11th - directives/diretivas-exercicios/src/App.vue
+++ 11th - directives/diretivas-exercicios/src/App.vue
@@ -7,13 +7,33 @@
 		<hr>
 		<p v-destaque.atrasar="'cyan'">Usando diretiva personalizada</p>
 		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada</p>
-		
+		<hr>
+		<p v-destaque-local.atrasar="'green'">Usando diretiva personalizada</p>
+		<p v-destaque-local:fundo.atrasar="'lightgreen'">Usando diretiva personalizada</p>	
+			
 	</div>
 </template>
 
 <script>
 export default {
+	directives:{
+		'destaque-local':{
+			bind(el, binding){
+		//el.style.backgroundColor = 'lightgreen'
+				let atraso = 0
 
+				if(binding.modifiers['atrasar'])
+					atraso = 3000
+				
+				setTimeout(()=>{
+					if(binding.arg === 'fundo')
+						el.style.backgroundColor = binding.value
+					else
+						el.style.color = binding.value
+				}, atraso)
+			}
+		}
+	}
 }
 </script>
 
