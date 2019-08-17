96fd8625e680d9d6adbe68589c8ddb9403a61d4d
Leandro <leaxviana140@gmail.com>
2019-08-17 01:43:45 -0400

    filtro cpf

--- 12th - filters-and-mixins/filters-and-mixins-exercises/src/App.vue
+++ 12th - filters-and-mixins/filters-and-mixins-exercises/src/App.vue
@@ -1,12 +1,27 @@
 <template>
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ CPF | filtroCPF }}</p>
 	</div>
 </template>
 
 <script>
 export default {
-
+	data(){
+		return{
+			CPF: '03249460129'
+		}
+	},
+	filters:{
+		filtroCPF(valor){
+			const arr = valor.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	}
 }
 </script>
 
