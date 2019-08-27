ea602903ef426492bd4b9b6a94515aca7412ab39
leandrocodes <leaxviana140@gmail.com>
2019-08-27 15:30:01 -0400

    filters and mixins challenge

--- 12th - filters-and-mixins/filters-and-mixins-challenge/src/App.vue
+++ 12th - filters-and-mixins/filters-and-mixins-challenge/src/App.vue
@@ -2,21 +2,22 @@
 	<div id="app">
 		<h1>Filtros & Mixins (Desafio)</h1>
 		<hr>
-		<p>{{trocaEspaco | trocaEspaco}}</p>
-		<div class="contadorDeString">
-			<p>{{"Leandro Viana Martins" | contadorString}}</p>
-		</div>
+		
+		
 		<!-- Exercício 1 -->
 		<!-- Construir um filtro local que troca espaços por vírgula -->
-		
+		<p>{{frase | espacoPorVirgula}}</p>
 		<!-- Exercício 2 -->
 		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
 		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
-
+		<div class="contadorDeString">
+			<p>{{"Leandro Viana Martins" | contadorString}}</p>
+		</div>
 		<!-- Exercício 3 -->
 		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
-
+		<p>{{espacoPorVirgulaComputada}}</p>
+		<p>{{contadorStringComputada}}</p>
 		<!-- Exercício 4 -->
 		<!-- Compartilhe a propriedade computada via mixin -->
 	</div>
@@ -26,13 +27,22 @@
 export default {
 	data(){
 		return{
-			trocaEspaco: 'Leandro Viana Martins',
+			frase: 'Leandro Viana Martins',
+			fraseComputada: 'Leandro Viana Martins',
 		}
 	},
 	filters:{
-		trocaEspaco(str){
-			let newString = str.replace(/[ ]+/g, ", ");
+		espacoPorVirgula(str){
+			let newString = str.replace(/ /g, ", ");
 			return newString
+		}
+	},
+	computed:{
+		espacoPorVirgulaComputada(){
+			return this.fraseComputada.replace(/ /g, ", ");
+		},
+		contadorStringComputada(){
+			return this.fraseComputada.split(' ').map(p => `${p} (${p.length})`).join(', ')
 		}
 	}
 }
--- 12th - filters-and-mixins/filters-and-mixins-challenge/src/main.js
+++ 12th - filters-and-mixins/filters-and-mixins-challenge/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 Vue.filter('contadorString', (string)=>{
-	
+	return string.split(' ').map(p => `${p} (${p.length})`).join(', ')
 })
 
 new Vue({
