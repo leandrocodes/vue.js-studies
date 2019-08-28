1aa622d7c7782a93c202d4105894c660a2497288
leandrocodes <leaxviana140@gmail.com>
2019-08-28 07:54:05 -0400

    adding classes

--- 13th - transitions and animations/animations-exercises/src/App.vue
+++ 13th - transitions and animations/animations-exercises/src/App.vue
@@ -1,13 +1,12 @@
 <template>
     <div id="app" class="container-fluid">
         <h1>Animações</h1>
-        <hr>
-	    <b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
-		
-		<transition>
-			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
-		</transition>	
-        
+        <hr />
+        <b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
+
+        <transition name="fade">
+            <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+        </transition>
     </div>
 </template>
 
@@ -31,5 +30,21 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
     font-size: 1.5rem;
+}
+
+.fade-enter {
+}
+
+.fade-enter-active {
+}
+
+.fade-enter-to {
+}
+
+.fade-leave {
+}
+.fade-leave-active {
+}
+.fade-leave-to {
 }
 </style>
