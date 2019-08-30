140de5fe25044c44aef473ec986c9243be3078b4
leandrocodes <leaxviana140@gmail.com>
2019-08-30 15:59:06 -0400

    interceptors

--- 14th - axios and firebase/http-exercises/src/plugins/axios.js
+++ 14th - axios and firebase/http-exercises/src/plugins/axios.js
@@ -4,11 +4,18 @@ import axios from 'axios'
 //axios.defaults.baseURL = 'https://vue-udemy-47d60.firebaseio.com/'
 
 Vue.use({
-    install(vue){
+    install(Vue) {
         //Vue.prototype.$http = axios
         Vue.prototype.axios = axios.create({
             baseURL: 'https://vue-udemy-47d60.firebaseio.com/'
 
+        })
+
+        Vue.prototype.axios.interceptors.request.use(config => {
+            console.log(config.method)
+            /*             if(config.method == 'post')
+                            config.method = 'put' */
+            return config
         })
     }
 })
