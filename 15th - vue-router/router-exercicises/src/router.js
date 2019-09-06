3c409b8bce627b0e9202a97a32c4428a6fd4a52f
leandrocodes <leaxviana140@gmail.com>
2019-09-06 15:47:01 -0400

    vue router configs

---
+++ 15th - vue-router/router-exercicises/src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Inicio from './components/Inicio'
+import Usuario from './components/Usuario/Usuario'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            component: Inicio
+        },
+        {
+            path: '/usuario',
+            component: Usuario
+        }
+    ]
+})
