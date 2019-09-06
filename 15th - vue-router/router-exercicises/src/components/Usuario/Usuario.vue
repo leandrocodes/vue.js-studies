c0725905bd59db653e388bc4b6a60bc0737dcfa5
leandrocodes <leaxviana140@gmail.com>
2019-09-06 18:35:55 -0400

    getting parameters and using in comps

--- 15th - vue-router/router-exercicises/src/components/Usuario/Usuario.vue
+++ 15th - vue-router/router-exercicises/src/components/Usuario/Usuario.vue
@@ -2,12 +2,18 @@
     <div class="usuario">
         <h2>Usuários</h2>
         <hr />
+        <p> <strong>Código</strong> {{id}}</p>
         <button sucesso @click="irInicio">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            id: this.$route.params.id
+        }
+    },
     methods: {
         irInicio(){
             this.$router.push('/')
