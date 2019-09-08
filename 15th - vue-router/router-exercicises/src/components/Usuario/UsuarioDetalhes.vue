9d341b1186ab16d6ccc63595f484652cb8ae5fc4
leandrocodes <leaxviana140@gmail.com>
2019-09-07 20:06:08 -0400

    route query params

--- 15th - vue-router/router-exercicises/src/components/Usuario/UsuarioDetalhes.vue
+++ 15th - vue-router/router-exercicises/src/components/Usuario/UsuarioDetalhes.vue
@@ -9,7 +9,7 @@
         <router-link
             tag="button"
             primario
-            :to="{ name: 'editarUsuario', params: { id } }"
+            :to="{ name: 'editarUsuario', params: { id }, query:{ completo: true, lingua: 'pt' } }"
         >Editar</router-link>
     </div>
 </template>
--- 15th - vue-router/router-exercicises/src/components/Usuario/UsuarioEditar.vue
+++ 15th - vue-router/router-exercicises/src/components/Usuario/UsuarioEditar.vue
@@ -3,7 +3,15 @@
         <h3>Usuário Editar</h3>
         <p>
             <strong>Código</strong>
-            {{id}}
+            {{ id }} /  {{ $route.params.id }}
+        </p>
+        <p>
+            <strong>Completo: </strong>
+            {{ $route.query.completo ? 'Sim': 'Não' }}
+        </p>
+        <p>
+            <strong>Língua: </strong>
+            {{ $route.query.lingua }}
         </p>
     </div>
 </template>
