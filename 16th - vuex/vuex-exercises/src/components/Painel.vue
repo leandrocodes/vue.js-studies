e0bfa4c0eb747713d9ec46fca54b43e8691e3fba
leandrocodes <leaxviana140@gmail.com>
2019-09-19 19:04:43 -0400

    prettier

--- 16th - vuex/vuex-exercises/src/components/Painel.vue
+++ 16th - vuex/vuex-exercises/src/components/Painel.vue
@@ -2,9 +2,7 @@
     <div class="painel">
         <div class="cabecalho">
             <span class="titulo">{{ titulo }}</span>
-            <span class="notificacao" v-if="notificacao">
-                {{ notificacao }}
-            </span>
+            <span class="notificacao" v-if="notificacao">{{ notificacao }}</span>
         </div>
         <div class="conteudo">
             <slot></slot>
@@ -19,47 +17,67 @@ export default {
 </script>
 
 <style scoped>
-    .painel {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        margin: 5px;
-    }
+.painel {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    margin: 5px;
+}
 
-    .cabecalho {
-        display: flex;
-        justify-content: center;
-    }
+.cabecalho {
+    display: flex;
+    justify-content: center;
+}
 
-    .titulo {
-        flex: 1;
-        padding: 5px;
-    }
+.titulo {
+    flex: 1;
+    padding: 5px;
+}
 
-    .conteudo {
-        padding: 25px;
-    }
+.conteudo {
+    padding: 25px;
+}
 
-    .notificacao {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 1.6rem;
-        font-weight: 600;
-        color: #FFF;
-        background-color: #e53935;
-        width: 60px;
-    }
+.notificacao {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.6rem;
+    font-weight: 600;
+    color: #fff;
+    background-color: #e53935;
+    width: 60px;
+}
 
-    .painel[vermelho] .cabecalho { color: #FFF; background-color: #c62828; }
-    .painel[vermelho] .conteudo { background-color: #f44336; }
+.painel[vermelho] .cabecalho {
+    color: #fff;
+    background-color: #c62828;
+}
+.painel[vermelho] .conteudo {
+    background-color: #f44336;
+}
 
-    .painel[verde] .cabecalho { color: #FFF; background-color: #2E7D32; }
-    .painel[verde] .conteudo { background-color: #4CAF50; }
-    
-    .painel[azul] .cabecalho { color: #FFF; background-color: #1565C0; }
-    .painel[azul] .conteudo { background-color: #2196F3; }
+.painel[verde] .cabecalho {
+    color: #fff;
+    background-color: #2e7d32;
+}
+.painel[verde] .conteudo {
+    background-color: #4caf50;
+}
 
-    .painel[roxo] .cabecalho { color: #FFF; background-color: #6A1B9A; }
-    .painel[roxo] .conteudo { background-color: #9C27B0; }
+.painel[azul] .cabecalho {
+    color: #fff;
+    background-color: #1565c0;
+}
+.painel[azul] .conteudo {
+    background-color: #2196f3;
+}
+
+.painel[roxo] .cabecalho {
+    color: #fff;
+    background-color: #6a1b9a;
+}
+.painel[roxo] .conteudo {
+    background-color: #9c27b0;
+}
 </style>
