25c28536ab32740b8beb1e56433da26f85b8e64c
leandrocodes <leaxviana140@gmail.com>
2019-10-23 08:17:27 -0400

    router add

--- 17th - stock trader/src/App.vue
+++ 17th - stock trader/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-	<h1>Stock Trader</h1>
+	<v-app>
+		<Header />
+		<v-content>
+			<v-container>
+				<router-view></router-view>
+			</v-container>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
+import Header from './components/Header'
 export default {
-
+	components: { Header }
 }
 </script>
 
