741ac847b6b7101b33e36023de58fdbd73130ab6
leandrocodes <leaxviana140@gmail.com>
2019-10-23 07:53:35 -0400

    portfolio components

---
+++ 17th - stock trader/src/components/portfolio/Portfolio.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Portfolio</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 17th - stock trader/src/components/portfolio/Stock.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Stock</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
