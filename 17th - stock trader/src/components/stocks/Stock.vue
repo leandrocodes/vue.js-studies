27e7a3f960c3b0173763604edd7791ab950e4769
leandrocodes <leaxviana140@gmail.com>
2019-10-23 09:13:41 -0400

    disable button at <=0

--- 17th - stock trader/src/components/stocks/Stock.vue
+++ 17th - stock trader/src/components/stocks/Stock.vue
@@ -12,7 +12,7 @@
     <v-card>
       <v-container fill-height>
         <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
-        <v-btn class="green darken-3 white--text" @click="buyStock">Comprar</v-btn>
+        <v-btn class="green darken-3 white--text" :disabled='quantity <= 0' @click="buyStock">Comprar</v-btn>
       </v-container>
     </v-card>
   </v-flex>
