9b5e1766439f5197eee092fb59373f1e7980485f
leandrocodes <leaxviana140@gmail.com>
2019-10-23 08:18:46 -0400

    toolbar

--- 17th - stock trader/src/components/Header.vue
+++ 17th - stock trader/src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Cabeçalho</h1>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase mr-4">
+      <span>Stock</span>
+      <span class="font-weight-light">Trader</span>
+    </v-toolbar-title>
+  </v-toolbar>
 </template>
 
 <script>
