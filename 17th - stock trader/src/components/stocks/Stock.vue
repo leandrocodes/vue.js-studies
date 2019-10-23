36d6d35039d0b7ab239ecd9cfc76ebc89d51ebc3
leandrocodes <leaxviana140@gmail.com>
2019-10-23 08:48:48 -0400

    stock component

--- 17th - stock trader/src/components/stocks/Stock.vue
+++ 17th - stock trader/src/components/stocks/Stock.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Ação</h1>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>NOME <small>R$: 2.00</small></strong>
+      </v-card-title>
+    </v-card>
+
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label='Quantidade' type='number'/>
+        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+      </v-container>
+    </v-card>
+
+  </v-flex>
 </template>
 
 <script>
