a647f8b70eb80e898ed179bb40495563124321ef
leandrocodes <leaxviana140@gmail.com>
2019-10-23 08:51:08 -0400

    stock hadcode

--- 17th - stock trader/src/components/stocks/Stocks.vue
+++ 17th - stock trader/src/components/stocks/Stocks.vue
@@ -3,8 +3,19 @@
 </template>
 
 <script>
+import Stock from './Stock'
 export default {
-
+  components: { Stock },
+  data () {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 200},
+        {id: 3, name: 'Apple', price: 280},
+        {id: 4, name: 'Twitter', price: 83},
+      ]
+    }
+  }
 }
 </script>
 
